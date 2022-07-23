155ab841b1f37a341a6cf09d9ecdeee08c115ded
slavkoss <slavkoss22@gmail.com>
2022-07-23 17:58:58 +0200

    ver 9.0.0.0 mnu, msg, mkd FUNCTIONAL namespaces, CRUD PDO, pretty URL-s

--- fwphp/www/home.php
+++ fwphp/www/home.php
@@ -9,7 +9,7 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
 
-        <title>Creative - Start Bootstrap Theme</title>
+        <title>B12phpfw</title>
 
         <!-- Favicon
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
